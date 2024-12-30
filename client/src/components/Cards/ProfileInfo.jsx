c02ececb31803e37998c0e3c3fa0467cb8f94c9e
Jenish <jenishkoladiya54@gmail.com>
2024-12-30 23:59:55 +0530

    first commit

---
+++ client/src/components/Cards/ProfileInfo.jsx
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+import { getInitials } from "../../utils/helper";
+
+const ProfileInfo = ({ onLogout, userInfo }) => {
+  return (
+    <div className="flex items-center gap-3">
+      <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">
+        {getInitials(userInfo?.username)}
+      </div>
+
+      <div>
+        <p className="text-sm font-medium">{userInfo?.username}</p>
+      </div>
+
+      <button
+        className="text-sm bg-red-500 p-1 rounded-md text-white hover:opacity-80"
+        onClick={onLogout}
+      >
+        Logout
+      </button>
+    </div>
+  );
+};
+ProfileInfo.propTypes = {
+  // onLogout: PropTypes.object,
+  userInfo: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+  }),
+};
+
+export default ProfileInfo;
